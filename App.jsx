c5bed02e5857f737e818574bde0c8b3944e8d355
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2024-12-30 20:40:52 +0545

    Learn to use component in react native

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Text,View,Button}from 'react-native'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import First from './src/components/First'
 
 const App = () => {
   return (
@@ -8,11 +8,13 @@ const App = () => {
    <Text style={{fontSize:30}}>React native mobile app developer</Text>
    <Text style={{fontSize:30}}>React native mobile app </Text>
    <Button title='Press Here' />
-
+   <First/>
     </View>
      
     
   )
 }
+
+
 
 export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text,View}from 'react-native'
+const First=()=>{
+    return (
+      <View>
+        <Text style= {{fontSize:20}}>First component</Text>
+    
+      </View>
+    )
+}
+
+export default First
