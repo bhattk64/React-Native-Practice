555285cfa1206534a97a40eeb58554826765ad77
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2024-12-30 20:01:32 +0545

    Learned about View,Text,Button component

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text,View,Button}from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
+const App = () => {
+  return (
+    <View>
+   <Text style={{fontSize:30}}>React native mobile app developer</Text>
+   <Text style={{fontSize:30}}>React native mobile app </Text>
+   <Button title='Press Here' />
+
+    </View>
+     
+    
+  )
+}
+
+export default App
