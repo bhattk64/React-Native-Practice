8facef0bf81849626a252d4c42e4fb5c234b20b3
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-02 16:11:46 +0545

    hide,show,toggle button functionalities

--- App.jsx
+++ App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 // import {Text,View,Button}from 'react-native'
 // import First from './src/components/First'
 import {View} from 'react-native'
-import UseEffectHookUnmountingPhase from './src/components/UseEffectHookUnmountingPhase'
+import HideShowToggle from './src/components/HideShowToggle'
+// import UseEffectHookUnmountingPhase from './src/components/UseEffectHookUnmountingPhase'
 // import UseEffectHookUpdatingPhase from './src/components/UseEffectHookUpdatingPhase'
 // import UseEffectHook from './src/components/UseEffectHook'
 // import ClassComponenets from './src/components/ClassComponenets'
@@ -40,7 +41,8 @@ const App = () => {
      {/* <ClassComponenets/> */}
      {/* <UseEffectHook/> */}
      {/* <UseEffectHookUpdatingPhase/> */}
-     <UseEffectHookUnmountingPhase/>
+     {/* <UseEffectHookUnmountingPhase/> */}
+     <HideShowToggle/>
     </View>
      
     
