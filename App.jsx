e7542a67618deb4027e21a6f37be9b67274ebd7f
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-12 00:14:48 +0530

    async storage in react native

--- App.jsx
+++ App.jsx
@@ -1,79 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import AsyncStorageExample from './src/components/AsyncStorageExample'
 
-const Tab=createMaterialTopTabNavigator()
-const chatScreen=()=>(
-  <View style={styles.container}>
-    <Text style={styles.text}>Chats</Text>
-  </View>
-)
-const statusScreen=()=>(
-  <View style={styles.container}>
-    <Text style={styles.text}>status</Text>
-  </View>
-)
-const callScreen=()=>(
-  <View style={styles.container}>
-    <Text style={styles.text}>call</Text>
-  </View>
-)
 const App = () => {
   return (
-   <NavigationContainer>
-   <Tab.Navigator
-   screenOptions={{
-    tabBarActiveTintColor:'#34A85A',
-    tabBarInactiveTintColor:'#000',
-    tabBarStyle:{
-      backgroundColor:'#fff',
-      height:50,
-      shadowColor:'#000',
-      shadowOffset:{width:0,height:2},
-      shadowOpacity:0.2,
-      shadowRadius:4,
-      elevation:5
-},
-tabBarIndicatorStyle:{
-  backgroundColor:'#34A85A',
-  height:3,
-  width:50,
-  borderRadius:3,
-  
-
-},
-tabBarLabelStyle:{
-  fontSize:12,
-  fontWeight:'bold',
-  marginHorizontal:10
-}
-   }}
-   
-   >
-    <Tab.Screen name="chats" component={chatScreen} />
-    <Tab.Screen name="Status" component={statusScreen} />
-    <Tab.Screen name="Calls" component={callScreen} />
-    
-   </Tab.Navigator>
-   </NavigationContainer>
+    <View>
+      <AsyncStorageExample/>
+    </View>
   )
 }
 
 export default App
-
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'#fff'
-
-  },
-  text:{
-    fontSize:20,
-    color:'#000',
-    fontWeight:'bold'
-
-  }
-})
---
+++ src/components/AsyncStorageExample.jsx
@@ -0,0 +1,48 @@
+import { View, Text, TextInput, Button, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const AsyncStorageExample = () => {
+    const [userName,setUserName]=useState('')
+    const[storedUsername,setStoredUsername]=useState('')
+    const saveUsername= async()=>{
+       await AsyncStorage.setItem('username',userName)
+       setUserName('')
+       Alert.alert('UserName Saved')
+    }
+    const getUsername= async()=>{
+      const savedUserName=  await AsyncStorage.getItem('username')
+        if(savedUserName){
+            setStoredUsername(savedUserName)
+
+        }
+        else{
+            Alert.alert('Username Not Found')
+        }
+       
+
+    }
+    const removeUsername=async()=>{
+        await AsyncStorage.removeItem('username')
+        setStoredUsername('')
+        Alert.alert('Username Removed')
+    }
+    useEffect(()=>{
+        getUsername()
+    },[])
+  return (
+    <View>
+        <TextInput
+        placeholder='Enter Username'
+        value={userName}
+        onChangeText={setUserName}
+        />
+        <Button title='Save username' onPress={saveUsername}/>
+        <Button title='Get username' onPress={getUsername} />
+        <Button  title='Remove username' onPress={removeUsername}/>
+      <Text>Stored Username:{storedUsername}</Text>
+    </View> 
+  )
+}
+
+export default AsyncStorageExample
