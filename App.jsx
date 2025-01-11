e1a6aa2d155c0521e3ded61aaac71c525cd4cf41
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-11 18:44:24 +0530

    Top tab navigation in react native

--- App.jsx
+++ App.jsx
@@ -1,57 +1,62 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
-const Tab=createBottomTabNavigator()
-const getTabBarIcon=(routeName,focused,color,size)=>{
-  let iconName
-  if(routeName==='Home'){
-    iconName=focused?'home':'home-outline'
-  }
-  else if(routeName==='Profile'){
-    iconName=focused?'person':'person-outline'
- }
- else if(routeName==='Settings'){ 
-  iconName=focused?'settings':'settings-outline'
- }
- return <Icon name={iconName} size={size} color={color} />
-
-}
-const HomeScreen=()=>(
+const Tab=createMaterialTopTabNavigator()
+const chatScreen=()=>(
   <View style={styles.container}>
-    <Text style={styles.text}>Home Screen</Text>
-
+    <Text style={styles.text}>Chats</Text>
   </View>
 )
-  const ProfileScreen=()=>(
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
-    </View>
-  )
-  const SettingsScreen=()=>(
-    <View style={styles.container}>
-      <Text style={styles.text}>Settings Screen</Text>
-    </View>
-  )
-
+const statusScreen=()=>(
+  <View style={styles.container}>
+    <Text style={styles.text}>status</Text>
+  </View>
+)
+const callScreen=()=>(
+  <View style={styles.container}>
+    <Text style={styles.text}>call</Text>
+  </View>
+)
 const App = () => {
   return (
-  <NavigationContainer>
-<Tab.Navigator
-screenOptions={({route})=>({
-  tabBarIcon:({focused,size,color})=>getTabBarIcon(route.name,focused,color,size),
-tabBarActiveTintColor:'#007Bff',
-tabBarInactiveTintColor:'#333',
-tabBarStyle:{backgroundColor:'#fff',height:60,paddingBottom:5}
-})}
-> 
-  <Tab.Screen  name='Home' component={HomeScreen} />
-  <Tab.Screen name='Profile' component={ProfileScreen} />
-  <Tab.Screen name='Settings' component={SettingsScreen} />
-</Tab.Navigator>
-  </NavigationContainer>
+   <NavigationContainer>
+   <Tab.Navigator
+   screenOptions={{
+    tabBarActiveTintColor:'#34A85A',
+    tabBarInactiveTintColor:'#000',
+    tabBarStyle:{
+      backgroundColor:'#fff',
+      height:50,
+      shadowColor:'#000',
+      shadowOffset:{width:0,height:2},
+      shadowOpacity:0.2,
+      shadowRadius:4,
+      elevation:5
+},
+tabBarIndicatorStyle:{
+  backgroundColor:'#34A85A',
+  height:3,
+  width:50,
+  borderRadius:3,
+  
+
+},
+tabBarLabelStyle:{
+  fontSize:12,
+  fontWeight:'bold',
+  marginHorizontal:10
+}
+   }}
+   
+   >
+    <Tab.Screen name="chats" component={chatScreen} />
+    <Tab.Screen name="Status" component={statusScreen} />
+    <Tab.Screen name="Calls" component={callScreen} />
+    
+   </Tab.Navigator>
+   </NavigationContainer>
   )
 }
 
@@ -62,22 +67,13 @@ const styles=StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems:'center',
-    backgroundColor:'#f0f0f0',
-
+    backgroundColor:'#fff'
 
   },
   text:{
     fontSize:20,
-    color:'#333',
-    fontWeight:'bold',
-    padding:10,
-    margin:10,
-    backgroundColor:'#fff',
-    borderRadius:10,
-    shadowColor:'#000',
-    shadowOffset:{width:0,height:2},
-    shadowOpacity:0.5,
-    shadowRadius:2,
-    elevation:2
-  },
+    color:'#000',
+    fontWeight:'bold'
+
+  }
 })
