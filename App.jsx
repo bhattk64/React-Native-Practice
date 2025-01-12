b1be487ba01f4aca776300180fde704984ad5456
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-12 23:46:22 +0530

    learned about redux toolkit in react native

--- App.jsx
+++ App.jsx
@@ -2,15 +2,23 @@ import { View } from 'react-native'
 import React from 'react'
 // import AsyncStorageExample from './src/components/AsyncStorageExample'
 // import ImageComponentExample from './src/components/ImageComponentExample'
-import Parent from './src/components/Parent'
+// import Parent from './src/components/Parent'
+import {Provider}from 'react-redux'
+import { store } from './src/redux/store/store'
+import Counter from './src/components/Counter'
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      {/* <AsyncStorageExample/> */}
-      {/* <ImageComponentExample/> */}
-      <Parent/>
-    </View>
+    // <View style={{flex:1}}>
+    //   {/* <AsyncStorageExample/> */}
+    //   {/* <ImageComponentExample/> */}
+    //   {/* <Parent/> */}
+    // </View>
+    <Provider store={store}>
+    <Counter/>
+
+
+    </Provider>
   )
 }
 
