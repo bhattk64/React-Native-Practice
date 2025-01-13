2fbf7b04372f4e14988ddcb660ef8c2ffc71b88d
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-14 00:09:36 +0530

    login form ui using nativewind(tailwind css)in react native

--- App.jsx
+++ App.jsx
@@ -1,26 +1,36 @@
-import { View } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import React from 'react'
+import MyLogin from './src/components/MyLogin'
+
+
+
 // import AsyncStorageExample from './src/components/AsyncStorageExample'
 // import ImageComponentExample from './src/components/ImageComponentExample'
 // import Parent from './src/components/Parent'
 // import {Provider}from 'react-redux'
 // import { store } from './src/redux/store/store'
 // import Counter from './src/components/Counter'
-import One from './src/components/One'
+// import One from './src/components/One'
 
 const App = () => {
+  
   return (
-    <View style={{flex:1}}>
-      {/* <AsyncStorageExample/> */}
-      {/* <ImageComponentExample/> */}
-      {/* <Parent/> */}
-      <One/>
-    </View>
+    // <View style={{flex:1}}>
+    //   {/* <AsyncStorageExample/> */}
+    //   {/* <ImageComponentExample/> */}
+    //   {/* <Parent/> */}
+    //   {/* <One/> */}
+     
+    // </View>
     // <Provider store={store}>
     // <Counter/>
 
 
     // </Provider>
+    <SafeAreaView className='flex-1'>
+      <MyLogin/>
+
+    </SafeAreaView>
     
   )
 }
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+const { getDefaultConfig } = require('metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-css-transformer'),
+  },
+  resolver: {
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'css'], // Add css if needed
+  },
+};
